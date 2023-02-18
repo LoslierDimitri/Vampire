33c5d748c10300325c9062ba6ef08d084a7af692
Dimitri <loslierdimitri@gmail.com>
2023-02-18 17:11:14 +0100

    Simple pathfinding added and clean
    Basic pathfinding target (player) follow done.
    
    Todo:
    Clear code and repack in functions.

--- Vampire_godot/Script/Npc/Npc_foe_01.gd
+++ Vampire_godot/Script/Npc/Npc_foe_01.gd
@@ -3,23 +3,35 @@ extends KinematicBody
 onready var main_node = get_tree().root.get_node("Main")
 
 onready var NAVIGATION_AGENT = get_node("NavigationAgent")
-#onready var TARGET = main_node.get_node("Map_actual").get_node("NavigationMeshInstance").get_node("TARGET")
-#onready var TARGET = main_node.get_node("Player_actual")
+
+export var speed = 5
+
+var target_reachable = false
+export var target_reachable_distance = 5
+var target_close = false
+export var target_close_distance = 5
 
 func _ready():
 	pass
-#	print(TARGET)
-#	return
-#	NAVIGATION_AGENT.set_target_location(TARGET.global_transform.origin)
 
 func _physics_process(delta):
 	var TARGET = main_node.get_node("Player_actual")
 	NAVIGATION_AGENT.set_target_location(TARGET.global_transform.origin)
 	var next = NAVIGATION_AGENT.get_next_location()
-	var velocity = (next - global_transform.origin).normalized() * 10 * delta
-#	move_and_collide(velocity)
-#	var snap_vector = -get_floor_normal()
-#	move_and_slide_with_snap(velocity, snap_vector, Vector3.UP)
-#	move_and_slide(velocity, Vector3.UP)
-#	move_and_collide(velocity)
-	translate(velocity)
+	var velocity = (next - global_transform.origin).normalized() * speed * delta
+	
+	var target_position = TARGET.global_transform.origin
+	var final_position = NAVIGATION_AGENT.get_final_location()
+	
+	if (target_position.distance_to(final_position) > target_reachable_distance):
+		target_reachable = false
+	else:
+		target_reachable = true
+	
+	if (target_position.distance_to(global_transform.origin) < target_close_distance):
+		target_close = true
+	else:
+		target_close = false
+	
+	if (target_reachable == true and target_close == false):
+		translate(velocity)
