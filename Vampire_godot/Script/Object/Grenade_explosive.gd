a97a90f4e4e00433c70b06349991a5249a086f1b
Dimitri <loslierdimitri@gmail.com>
2023-02-21 11:53:32 +0100

    Grenade explosion, grenade stun and gun done
    Ennemy take damage.
    
    Todo:
    Mine explosion and mine stun.

--- Vampire_godot/Script/Object/Grenade_explosive.gd
+++ Vampire_godot/Script/Object/Grenade_explosive.gd
@@ -7,11 +7,10 @@ export var scale_object = 0.1
 onready var MESH = get_node("MeshInstance")
 onready var COLLISION = get_node("CollisionShape")
 
-onready var RAYCAST = get_node("RayCast")
-
 onready var TIMER = get_node("Timer")
 
-#var raycast
+onready var RANGE_EXPLOSION = get_node("Range_explosion")
+
 export var type = "damage"
 export var damage = 50
 export var explosion_range = 5
@@ -23,6 +22,9 @@ func _ready():
 	COLLISION.scale.x = scale_object
 	COLLISION.scale.y = scale_object
 	COLLISION.scale.z = scale_object
+	RANGE_EXPLOSION.scale.x = explosion_range
+	RANGE_EXPLOSION.scale.y = explosion_range
+	RANGE_EXPLOSION.scale.z = explosion_range
 	
 	TIMER.start()
 
@@ -31,35 +33,29 @@ func damage():
 	var npc_foe_node = map_node.get_node("Npc_foe")
 	var npc_neutral_node = map_node.get_node("Npc_neutral")
 	
-#	raycast = RayCast.new()
-	RAYCAST.global_transform.origin = global_transform.origin
-	RAYCAST.global_transform.origin.y += 2
-#	raycast.translation = global_transform.origin
-	
 	explosion(npc_foe_node)
 	explosion(npc_neutral_node)
 
 func explosion(npc_node):
 	for node in npc_node.get_children():
-#		RAYCAST.set_cast_to((node.global_transform.origin - RAYCAST.global_transform.origin) * 100)
-		RAYCAST.set_cast_to((RAYCAST.global_transform.origin - node.global_transform.origin) * 100)
-#		RAYCAST.set_cast_to(node.global_transform.origin)
-#		raycast.global_rotation = node.global_transform.origin - raycast.global_transform.origin
-		RAYCAST.force_raycast_update()
-#		RAYCAST.set_cast_to(RAYCAST.get_cast_to().normalized() * 100)
-		var collider = RAYCAST.get_collider()
+		if (node.global_transform.origin.distance_to(global_transform.origin) <= explosion_range):
+				if (node.is_in_group("Can_take_damage") == true):
+					node.take_damage(damage, type)
 		
-		print("grenade position: ", global_transform.origin)
-		print("raycast position: ", RAYCAST.global_transform.origin)
-		print("npc position: ", node.global_transform.origin)
-		print("raycast cast to: ", RAYCAST.get_cast_to())
-		print("collider: ", collider)
-#		print("collider position: ", collider.global_transform.origin)
-		print("node: ", node)
 		
-		if (collider == node):
-			if (node.is_in_group("Can_take_damage") == true):
-				node.take_damage(damage, type)
+#		var space_state = get_world().direct_space_state
+#		var result = space_state.intersect_ray(global_transform.origin, node.global_transform.origin)
+#
+#		if (result):
+#			var collider = result.collider
+#
+#			if (collider.global_transform.origin.distance_to(global_transform.origin) <= explosion_range):
+#				if (collider.is_in_group("Can_take_damage") == true):
+#					collider.take_damage(damage, type)
+			
+#			if (collider.global_transform.origin.distance_to(global_transform.origin) <= explosion_range):
+#				if (collider.is_in_group("Can_take_damage") == true):
+#					collider.take_damage(damage, type)
 
 func _on_Timer_timeout():
 	damage()
--- Vampire_godot/Script/Object/Gun.gd
+++ Vampire_godot/Script/Object/Gun.gd
@@ -1,7 +1,24 @@
 extends StaticBody
 
+onready var main_node = get_tree().root.get_node("Main")
+var map_node
+var player_node
+var gun_position
+
+export var damage = 40
+export var type = "damage"
+
 func _ready():
-	pass
+	player_node = main_node.get_node("Player_actual")
 
 func damage(gun_list):
-	pass
+	for node in gun_list:
+		var space_state = get_world().direct_space_state
+		gun_position = player_node.get_node("Camera_pivot").get_node("Camera").global_transform.origin
+		var result = space_state.intersect_ray(gun_position, node.global_transform.origin)
+		
+		if (result):
+			print(result.collider)
+			if (result.collider == node):
+				if (node.is_in_group("Can_take_damage") == true and node.is_in_group("Player") == false):
+					node.take_damage(damage, type)
