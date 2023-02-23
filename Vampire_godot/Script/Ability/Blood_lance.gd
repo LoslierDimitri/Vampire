fac67d356df73a4bd2143ea1d5ee72d0cf35410b
Dimitri <loslierdimitri@gmail.com>
2023-02-23 19:28:35 +0100

    Ability blood lance not done yet
    Todo:
    Clean code of object, create object to instance with proper model. Rewrite functions to always be in proper tscn files.

---
+++ Vampire_godot/Script/Ability/Blood_lance.gd
@@ -0,0 +1,32 @@
+extends Spatial
+
+onready var main_node = get_tree().root.get_node("Main")
+
+onready var BLOOD_LANCE = load("res://Model/Ability/Blood_lance.tscn")
+
+export var type = "damage"
+export var damage = 50
+
+export var timer = 2
+
+func damage(raycast):
+	raycast.force_raycast_update()
+	if !raycast.is_colliding():
+		return
+	
+#	var position = raycast.get_collision_point()
+	var blood_lance_instance = BLOOD_LANCE.instance()
+#	collider.add_child(blood_lance_instance)
+	
+	var collider = raycast.get_collider()
+#	print(collider)
+	var collision_position = raycast.get_collision_point()
+	if (collider.is_in_group("Can_take_damage") == true):
+		collider.take_damage(damage, type)
+		collider.add_child(blood_lance_instance)
+	else:
+		blood_lance_instance.global_transform.origin = collision_position
+#		blood_lance_instance.look_at(main_node.get_node("Player_actual").global_transform.origin - blood_lance_instance.global_transform.origin, Vector3.UP)
+		main_node.add_child(blood_lance_instance)
+	
+	
--- Vampire_godot/Script/Player/Ability.gd
+++ Vampire_godot/Script/Player/Ability.gd
@@ -1,4 +1,39 @@
 extends Spatial
 
+onready var main_node = get_tree().root.get_node("Main")
+onready var player_node = main_node.get_node("Player_actual")
+
+onready var POSITION_LAUNCH = player_node.get_node("Camera_pivot").get_node("Camera").get_node("Position_launch")
+onready var POSITION_LAUNCH_TARGET = player_node.get_node("Camera_pivot").get_node("Camera").get_node("Position_launch_target")
+onready var RAYCAST = player_node.get_node("Camera_pivot").get_node("Camera").get_node("Raycast")
+
+onready var BLOOD_LANCE = load("res://Ability/Blood_lance.tscn")
+
+var blood_lance_instance
+var blood_lance
+
+func _ready():
+	blood_lance_instance = BLOOD_LANCE.instance()
+	blood_lance_instance.name = "blood_lance"
+	blood_lance_instance.global_transform.origin = Vector3(0, -200, 0)
+	add_child(blood_lance_instance)
+	blood_lance = get_node("blood_lance")
+
 func action(ability_name):
-	pass
+	if (ability_name == "Blood_lance"):
+		print("action blood_lance")
+		
+		blood_lance.damage(RAYCAST)
+		
+	if (ability_name == "Vampire_vision"):
+		print("action vampire_vision")
+		
+	if (ability_name == "Hypnosis"):
+		print("action hypnosis")
+		
+	if (ability_name == "Shadow_form"):
+		print("action shadow_form")
+		
+	if (ability_name == "Blood_link"):
+		print("action blood_link")
+		
